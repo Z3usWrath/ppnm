8cafc617fde477470a3c3ef38b79a6e23450619a
Z3usWrath <karishberg@gmail.com>
2023-03-08 12:52:24 +0100

    list

---
+++ exercises/generic/genlist.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class genlist<T>{
+	public T[] data;
+	public int size => data.Length; // property
+	public T this[int i] => data[i]; // indexer
+	public genlist(){ data = new T[0]; }
+	public void add(T item){ /* add item to the list */
+		T[] newdata = new T[size+1];
+		System.Array.Copy(data,newdata,size);
+		newdata[size]=item;
+		data=newdata;
+	}
+}
