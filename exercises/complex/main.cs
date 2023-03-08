750976ac6eaca963716cf2be9133e066638e1d7b
Z3usWrath <karishberg@gmail.com>
2023-03-08 10:03:14 +0100

    complex_final

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -6,19 +6,25 @@ class main{
 	public static int Main(){
 	complex j = new complex(-1,0);
 	complex x = new complex(0,1);
+	complex s = new complex(0,-1);
 	complex y = new complex(0,Math.PI);
-	Write($"\u221A(-1) = "+cmath.sqrt(j)+"\n");
-	Write($"We recieved the sqrt of -1 "+complex.approx(-1,x,1e-3,1e-3)+" \n");
-	Write($"\u221A(i) = "+cmath.sqrt(x)+"\n");
+	complex sqrti = new complex(Math.Cos(Math.PI/4),Math.Sin(Math.PI/4));
+
+	if(sqrti.approx(cmath.sqrt(x))){
+	Write($"After checking we get \u221A(i) = "+cmath.sqrt(x)+"\n");
+	}	
 	Write($"exp(i) = "+cmath.exp(x)+"\n");
-	Write($"exp(i*pi) = "+cmath.exp(y)+"\n");
-	Write($"exp(i^i) = "+cmath.pow(x,x)+"\n");
+	if(cmath.exp(y).approx(j)){
+	Write($"After checking we get Exp(i*PI) = "+cmath.exp(y)+"\n");
+	}
+	Write($"exp(i^i) = "+cmath.pow(x,x)+" = Exp(-Pi/2)\n");
 	Write($"log(i) = "+cmath.log(x)+"\n");
 	Write($"sin(i*pi) = "+cmath.sin(y)+"\n");
-	if(complex.approx(cmath.sqrt(x),y,1e-9,1e-9)){
-	Write("Im in\n");
+	if(x.approx(cmath.sqrt(j)) && j.approx(cmath.pow(s,2))){
+	Write($"After checking we get \u221A(-1)=\u00B1i\n");
+		
 	}	
-
+	
 	return 0;
         }
 }
